a0c0b4cfeb520784037578ebbf46cc3708568039
willamy388720 <willamygt@gmail.com>
2020-04-25 12:49:59 -0300

    Relacionamentos

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,7 @@ class ContactsController < ApplicationController
 
   # GET /contacts/1
   def show
-    render json: @contact, include: [:kind] #, include: [:kind, :phones, :address]
+    render json: @contact, include: [:kind, :phones, :address] #, include: [:kind, :phones, :address]
   end
 
   # POST /contacts
@@ -46,6 +46,7 @@ class ContactsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def contact_params
-      params.require(:contact).permit(:name, :email, :birthdate, :kind_id, phones_attributes: [:id, :number, :_destroy], address_attributes: [:id, :street, :city])
+      # params.require(:contact).permit(:name, :email, :birthdate, :kind_id, phones_attributes: [:id, :number, :_destroy], address_attributes: [:id, :street, :city])
+      ActiveModelSerializers::Deserialization.jsonapi_parse(params)
     end
 end
