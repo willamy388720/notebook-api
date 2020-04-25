de5c74323048fe69ed2ce4f3775306e97fed567e
willamy388720 <willamygt@gmail.com>
2020-04-25 10:57:26 -0300

    Media Types

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,12 +2,12 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
   belongs_to :kind do
-    link(:kind) { kind_url(object.kind.id) }
+    link(:related) { kind_url(object.kind.id) }
   end
   has_many :phones
   has_one :address
 
-  link(:self) { contact_url(object.id) }
+  # link(:self) { contact_url(object.id) }
 
   def attributes(*args)
     h = super(*args)
