176fc0f863749303b17f642a590e6085eceee8de
willamy388720 <willamygt@gmail.com>
2020-04-25 10:29:22 -0300

    AMS e links

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,6 +1,14 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
+  belongs_to :kind do
+    link(:kind) { kind_url(object.kind.id) }
+  end
+  has_many :phones
+  has_one :address
+
+  link(:self) { contact_url(object.id) }
+
   def attributes(*args)
     h = super(*args)
     # h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
